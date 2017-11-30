9c60da62a982438361d0643dc5c689148dcb959d
Eric McConkie <eric@poynt.co>
2017-11-30 13:04:56 -0800

    config sample

---
+++ config.js
@@ -0,0 +1,7 @@
+//you can hardcode these, or pass them in the command line:
+// node index dir=path/to/your/gpx/files cookie=youMovesCountCookieAfterLogin
+
+module.exports = {
+  path: 'XXX_PATH_TO_GPXs____XXX',
+  cookie: 'XXXSUPERLONGCOOKIEXXX'
+};
